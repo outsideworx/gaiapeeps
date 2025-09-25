3db23a6f15aeb44c1c823d8ed5ed1a3556d262ed
hornokattila <hornokattila@outlook.com>
2025-09-25 23:23:17 +0200

    Spaces.

--- videos.js
+++ videos.js
@@ -1,28 +1,27 @@
-  function loadVideos() {
+function loadVideos() {
     $.ajax({
-      url: `/api/videos`,
-      method: "GET",
-      success: function(videos) {
-        console.log("LOG: response - ", videos);
-        const container = $("#videos");
-        container.empty();
-        videos.forEach(video => {
-          const iframe = $("<iframe>", {
-            height: "560",
-            src: video.link,
-            title: video.title,
-            frameborder: "0",
-            allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
-            referrerpolicy: "strict-origin-when-cross-origin",
-            allowfullscreen: true
-          });
-          container.append(iframe);
-        });
-      },
-      error: function(xhr, status, error) {
-        console.error("Error loading videos:", status, error);
-      }
+        url: `/api/videos`,
+        method: "GET",
+        success: function(videos) {
+            console.log("LOG: response - ", videos);
+            const container = $("#videos");
+            container.empty();
+            videos.forEach(video => {
+                const iframe = $("<iframe>", {
+                    height: "560",
+                    src: video.link,
+                    title: video.title,
+                    frameborder: "0",
+                    allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+                    referrerpolicy: "strict-origin-when-cross-origin",
+                    allowfullscreen: true
+                });
+                container.append(iframe);
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error("Error loading videos:", status, error);
+        }
     });
-  }
 
-  $(document).ready(loadVideos);
+$(document).ready(loadVideos);
